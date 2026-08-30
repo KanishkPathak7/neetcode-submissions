d9439ef446fefa117970577c807f49f96aa42b30
Kanishk Pathak <kanishkpathak10@gmail.com>
2026-08-30 18:59:47 +0530

    Added Problem Description

--- Data Structures & Algorithms/duplicate-integer/submission-1.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -1,3 +1,10 @@
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+// Input: nums = [1, 2, 3, 3]
+
+// Output: true
+
+
+
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
@@ -10,3 +17,4 @@ public:
         return false;
     }
 };
+
